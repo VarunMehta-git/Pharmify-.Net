b8d496c9d5c214425c49098792c9c549aff766a8
VarunMehta-git <127774462+VarunMehta-git@users.noreply.github.com>
2024-04-04 23:45:00 +0530

    Add files via upload

---
+++ wwwroot/js/SearchMenu.js
@@ -0,0 +1,18 @@
+﻿document.getElementById('srcMenu').addEventListener('click', function () {
+    var searchQuery = document.querySelector('.search').value.trim().toLowerCase();
+
+    var recipeTitles = document.querySelectorAll('.content-title h5:first-child');
+
+    recipeTitles.forEach(function (title) {
+        var recipeName = title.textContent.trim().toLowerCase();
+        var parentContainer = title.closest('.content-wrapper');
+
+        // Check if the recipe name includes the search query
+        if (recipeName.includes(searchQuery)) {
+            parentContainer.style.display = 'block'; 
+        } else {
+            parentContainer.style.display = 'none'; 
+        }
+    });
+});
+//
